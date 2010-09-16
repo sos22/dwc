990683f334f5abb4c8c82aafdadee5fcd28a9c43
Steven Smith <sos22@cl.cam.ac.uk>
2010-09-16 15:09:00 +0100

    Tweak hash function again.

--- dwc.h
+++ dwc.h
@@ -6,7 +6,7 @@ struct word {
 	unsigned char word[];
 };
 
-#define NR_HASH_TABLE_SLOTS 131071
+#define NR_HASH_TABLE_SLOTS 262143
 extern struct word * hash_table[NR_HASH_TABLE_SLOTS];
 
 void *bump_malloc(size_t s);
